4f27415e7319aca37fa750839a886a3baa7301aa
julienleonard <jl.julien.leonard@gmail.com>
2015-10-01 16:29:38 +1000

    Update polygon with Point2D

--- app/src/main/java/julienl/androidgl/Polygon.java
+++ app/src/main/java/julienl/androidgl/Polygon.java
@@ -5,64 +5,64 @@ package julienl.androidgl;
  */
 public class Polygon {
 
-    Point[] mPoints;
+    Point2D[] mPoint2Ds;
 
     public Polygon() {
     }
 
-    public Polygon(Point[] points) {
-        mPoints = points;
+    public Polygon(Point2D[] point2Ds) {
+        mPoint2Ds = point2Ds;
     }
 
     public Polygon(float[] coords) {
-        mPoints = new Point[coords.length/2];
+        mPoint2Ds = new Point2D[coords.length/2];
         for (int index = 0; index < coords.length/2; index += 1) {
-            mPoints[index] = new Point(coords[2*index],coords[2*index+1]);
+            mPoint2Ds[index] = new Point2D(coords[2*index],coords[2*index+1]);
         }
     }
 
-    public Point[] points() {
-        return mPoints;
+    public Point2D[] points() {
+        return mPoint2Ds;
     }
 
     public float[] coords() {
-        float[] result = new float[mPoints.length * 2];
+        float[] result = new float[mPoint2Ds.length * 2];
         int index = 0;
-        for (Point p : mPoints) {
-            result[2 * index] = p.x();
-            result[2 * index + 1] = p.y();
+        for (Point2D p : mPoint2Ds) {
+            result[2 * index] = (float)p.x();
+            result[2 * index + 1] = (float)p.y();
             index += 1;
         }
         return result;
     }
 
-    public Point middle() {
+    public Point2D middle() {
         float sumx = 0.0f;
         float sumy = 0.0f;
-        for (Point p : mPoints) {
+        for (Point2D p : mPoint2Ds) {
             sumx += p.x();
             sumy += p.y();
         }
-        float fnpoints = (float)mPoints.length;
+        float fnpoints = (float) mPoint2Ds.length;
 
-        return new Point(sumx/fnpoints,sumy/fnpoints);
+        return new Point2D(sumx/fnpoints,sumy/fnpoints);
     }
 
     //
     // basic algo : works only for convex polys
     //
     public float[] GLCoords() {
-        float[] result = new float[(mPoints.length + 1) * 3];
-        Point pmiddle = middle();
+        float[] result = new float[(mPoint2Ds.length + 1) * 3];
+        Point2D pmiddle = middle();
         short index = 0;
-        result[3 * index]     = pmiddle.x();
-        result[3 * index + 1] = pmiddle.y();
+        result[3 * index]     = (float)pmiddle.x();
+        result[3 * index + 1] = (float)pmiddle.y();
         result[3 * index + 2] = 0.0f;
         index = +1;
 
-        for (Point p : mPoints) {
-            result[3 * index]     = p.x();
-            result[3 * index + 1] = p.y();
+        for (Point2D p : mPoint2Ds) {
+            result[3 * index]     = (float)p.x();
+            result[3 * index + 1] = (float)p.y();
             result[3 * index + 2] = 0.0f;
             index += 1;
         }
@@ -73,9 +73,9 @@ public class Polygon {
     // basic algo : works only for convex polys
     //
     public short[] GLOrder() {
-        short[] result = new short[mPoints.length * 3];
+        short[] result = new short[mPoint2Ds.length * 3];
 
-        for (short pindex = 0; pindex < mPoints.length-1; pindex++) {
+        for (short pindex = 1; pindex < mPoint2Ds.length; pindex++) {
             result[pindex * 3]     = 0; // middle point
             result[pindex * 3 + 1] = pindex;
             result[pindex * 3 + 2] = (short) (pindex + 1);
