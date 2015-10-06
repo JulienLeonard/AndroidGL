cebd820ade52e5689582beb155b03ac1e2de5c94
julienleonard <jl.julien.leonard@gmail.com>
2015-10-06 17:07:57 +1100

    Add distance function

--- app/src/main/java/julienl/androidgl/Point2D.java
+++ app/src/main/java/julienl/androidgl/Point2D.java
@@ -19,4 +19,14 @@ public class Point2D {
     public Point2D add(Vector2D v) {
         return new Point2D(mx + v.x(), my + v.y());
     }
+
+    public static double distance2(Point2D p1, Point2D p2) {
+        double x = p1.x() - p2.x();
+        double y = p1.y() - p2.y();
+        return (x * x + y * y);
+    }
+
+    public static double distance(Point2D p1, Point2D p2) {
+        return Math.sqrt(Point2D.distance(p1,p2));
+    }
 }
